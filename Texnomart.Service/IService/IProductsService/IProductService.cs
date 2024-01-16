e424fd315288cc3f4d1d3cf4a3a58155dd3bcaa6
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-01-16 19:25:49 +0500

    Inerface Service

---
+++ Texnomart.Service/IService/IProductsService/IProductService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Texnomart.Domain.Entity.Products;
+using Texnomart.Service.Dtos.ProductsDto;
+
+namespace Texnomart.Service.IService.IProductsService
+{
+    public interface IProductService
+    {
+        IQueryable<Product> GetAll(Expression<Func<Product, bool>> expression, string[] includes = null);
+        ValueTask<ProductDto> GetAsync(Expression<Func<Product, bool>> expression, string[] includes = null);
+        ValueTask<ProductDto> CreateAsync(ProductDto entity);
+        ValueTask<bool> DeleteAsync(Expression<Func<Product, bool>> expression);
+        ProductDto Update(int id, ProductDto entity);
+    }
+}
---
+++ Texnomart.Service/IService/IUsersService/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Texnomart.Domain.Entity.Users;
+using Texnomart.Service.Dtos.UsersDto;
+
+namespace Texnomart.Service.IService.IUsersService
+{
+    public interface IUserService
+    {
+        IQueryable<User> GetAll(Expression<Func<User, bool>> expression, string[] includes = null);
+        ValueTask<UserDto> GetAsync(Expression<Func<User, bool>> expression, string[] includes = null);
+        ValueTask<UserDto> CreateAsync(UserDto entity);
+        ValueTask<bool> DeleteAsync(Expression<Func<User, bool>> expression);
+        UserDto Update(int id, UserDto entity);
+    }
+}
