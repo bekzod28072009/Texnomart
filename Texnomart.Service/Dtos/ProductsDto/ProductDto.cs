82c34dcc0db36f563213ddb098c523d6bc28979e
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-01-16 18:59:44 +0500

    Dto

---
+++ Texnomart.Service/Dtos/ProductsDto/ProductDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Texnomart.Service.Dtos.ProductsDto
+{
+    public class ProductDto
+    {
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public string Country { get; set; }
+
+        public string? Model { get; set; }
+
+        public string? Size { get; set; }
+
+        public string? Quality { get; set; }
+    }
+}
