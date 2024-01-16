0ebdd938aaaf41b1fc60a7b995c4f941399219ff
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-01-16 18:28:20 +0500

    Database

---
+++ Texnomart.Data/AppDBContexts/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Texnomart.Domain.Entity.Products;
+using Texnomart.Domain.Entity.Users;
+
+namespace Texnomart.Data.AppDBContexts
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base (options) 
+        { }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Texnomart/Extension/Middleware.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Texnomart.Data.AppDBContexts;
+
+namespace Texnomart.Extension
+{
+    public static class Middleware
+    {
+        public static void AddDbConTextes(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDBContext>(option =>
+            option.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
